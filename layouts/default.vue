1b8bc3a3545d3dd6c57b381a5a1b944bae61d760
Dennis <dennis-107@hotmail.com>
2021-03-01 21:59:28 +0100

    sass loader

--- layouts/default.vue
+++ layouts/default.vue
@@ -47,7 +47,7 @@ export default {
   },
 };
 </script>
-<style>
+<style lang="scss">
 @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap');
 html {
   font-size: 16px;
